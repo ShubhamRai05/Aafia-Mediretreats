74aeee32f5db03c157112157f0f17180effa6734
Shubham Rai <shubham56237@gmail.com>
2023-09-02 23:58:20 +0530

    header navigation created

--- js/google-translator.js
+++ js/google-translator.js
@@ -1,6 +1,20 @@
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement(
+//         { pageLanguage: 'en' ,
+//         layout : google.translate.TranslateElement.InlineLayout.SIMPLE},
+//         'google_translate_element'
+//     );
+// }
 function googleTranslateElementInit() {
     new google.translate.TranslateElement(
-        { pageLanguage: 'en' },
+        { 
+            pageLanguage: 'en',
+            includedLanguages: 'es,fr,de', // Include additional languages
+            layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL, // Choose layout style
+            gaTrack: true, // Enable Google Analytics tracking (if you have GA set up)
+            gaId: 'YOUR_GA_ID', // Replace with your Google Analytics ID
+            autoDisplay: false, // Disable automatic language display
+        },
         'google_translate_element'
     );
 }

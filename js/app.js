a34fc4d6a010f3dee3737ab66bfa7a3bc34c3e7b
Shubham Rai <shubham56237@gmail.com>
2023-09-03 21:19:38 +0530

    hospital carousel created

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
 let enquiryForm = document.querySelector(".enquiry-form");
-let treatmentOverview = document.getElementById("treatment-overview");
-
 let formOffsetTop = enquiryForm.offsetTop;
+let doctorsSection = document.querySelector(".our-doctors");
+let doctorsSectionOffsetTop = doctorsSection.offsetTop;
 
 // Function to handle the scroll event
 function handleScroll() {
     // Get the current scroll position
     let scrollPosition = window.scrollY;
 
-    // Check if the user has scrolled to or beyond the form's offset position
-    if (scrollPosition >= formOffsetTop) {
-        // Add a CSS class to make the form fixed
+    if (scrollPosition >= formOffsetTop && scrollPosition + enquiryForm.offsetHeight < doctorsSectionOffsetTop) {
         enquiryForm.classList.add("fixed-form");
     } else {
-        // Remove the CSS class to return the form to its previous position
+
         enquiryForm.classList.remove("fixed-form");
     }
 }
 
 window.addEventListener("scroll", handleScroll);
+
 
 //  FAQ TOGGLE
 const accordian = document.querySelectorAll(".accordian")
@@ -27,13 +26,12 @@ let answer = document.querySelector(".answer")
 accordian.forEach((accord) => {
     const icon = accord.querySelector(".icon")
     const answer = accord.querySelector(".answer")
-    icon.addEventListener("click", () => {
+    accord.addEventListener("click", () => {
         if (answer.classList.toggle("active-faq") && icon.classList.contains("fa-plus")) {
             icon.classList.remove("fa-plus")
             icon.classList.add("fa-minus")
 
-        }else
-        {
+        } else {
             icon.classList.add("fa-plus")
         }
 
@@ -41,8 +39,53 @@ accordian.forEach((accord) => {
 
 })
 
+// owl carousel
+
+$('.owl-two').owlCarousel({
+    loop: true,
+    margin: 40,
+    nav: true,
+    dots:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+// owl one
+$('.owl-one').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+    
+        1000: {
+            items: 2
+        }
+    }
+})
 
 
+// owl three
+
+$('.owl-three').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+    
+        1000: {
+            items: 3
+        }
+    }
+})
 
 
 
